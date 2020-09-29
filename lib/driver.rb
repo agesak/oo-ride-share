84bdc279fbd310fa85df7e218042046cf7fe9896
Kareha Agesa <kmaagesa@gmail.com>
2020-09-29 16:22:03 -0700

    adding from_csv to driver

---
+++ lib/driver.rb
@@ -0,0 +1,39 @@
+require_relative "csv_record"
+
+module RideShare
+  class Driver < RideShare::CsvRecord
+    attr_reader :id, :name, :vin, :status, :trips
+
+    def initialize(id:, name:, vin:, status: :AVAILABLE, trips: [])
+      super(id)
+
+      @name = name
+      @vin = validate_vin(vin)
+      @status = validate_status(status)
+      @trips = trips
+    end
+
+    private
+
+    def validate_vin(vin)
+       raise ArgumentError.new("VIN must be 17 characters") if vin.length != 17
+       return vin
+    end
+
+    def validate_status(status)
+      status = status.to_sym
+      raise ArgumentError.new("Status must be :AVAILABLE or :UNAVAILABLE") unless [:AVAILABLE, :UNAVAILABLE].include?(status)
+      return status
+    end
+
+    def self.from_csv(record)
+      return new(
+        id: record[:id],
+        name: record[:name],
+        vin: record[:vin],
+        status: record[:status]
+      )
+    end
+
+  end
+end
